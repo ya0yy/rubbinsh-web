c4596f389f6cd0487d11fc2abf3f4458ab3bd1a6
yaoyy <tuzkiyao@gmail.com>
2019-01-18 09:45:09 +0800

    圆角处理、导航栏现在是响应式了

--- src/view/index.vue
+++ src/view/index.vue
@@ -1,28 +1,48 @@
 <template>
+    <!-- 布局  gutter：每列的间隔 -->
     <el-row :gutter="20">
 
-        <el-col :span="6" offset="3">
+        <el-col :sm="6" :offset="4" :xs="{span: 24, offset: 0}">
             <div class="grid-content bg-purple">
-                <div style="width: 100%; ">
-                    <div style="width: 400px; margin:50px auto 0px auto; ">
-                        <el-carousel :interval="4000" height="300px">
-                            <el-carousel-item v-for="item in 5" :key="item">
-                                <img src="../img/test.jpg" alt="实例图片" width="400">
-                            </el-carousel-item>
-                        </el-carousel>
-                    </div>
-                </div>
+                <el-carousel :interval="4000" height="300px">
+                    <el-carousel-item v-for="item in 5" :key="item">
+                        <img src="../img/test.jpg" alt="实例图片" width="455">
+                    </el-carousel-item>
+                </el-carousel>
             </div>
         </el-col>
-        <el-col :span="12">
-            <div class="grid-content bg-purple">
-                <div style="width: 100%; ">
-                    <div style="width: 400px; margin:50px auto 0px auto; ">
 
-                    </div>
+        <el-col :sm="10" :xs="{span: 24}">
+            <div class="grid-content bg-purple">
+                <div>
+                    <el-table
+                            :data="tableData"
+                            style="width: 100%"
+                            :default-sort = "{prop: 'date', order: 'descending'}"
+                            highlight-current-row="true"
+                    >
+                        <el-table-column
+                                prop="date"
+                                label="日期"
+                                sortable
+                                width="180">
+                        </el-table-column>
+                        <el-table-column
+                                prop="name"
+                                label="姓名"
+                                sortable
+                                width="180">
+                        </el-table-column>
+                        <el-table-column
+                                prop="address"
+                                label="地址"
+                                :formatter="formatter">
+                        </el-table-column>
+                    </el-table>
                 </div>
             </div>
         </el-col>
+
     </el-row>
 </template>
 <script>
@@ -31,43 +51,44 @@
 
         },
         data() {
-            return {}
+            return {tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
+        },
+        methods: {
+            formatter(row, column) {
+                return row.address;
+            }
         }
     }
 </script>
 <style scoped>
-    .el-row {
-        margin-bottom: 20px;
 
-    &
-    :last-child {
-        margin-bottom: 0;
-    }
-
-    }
+    /* 每一列，圆角 */
     .el-col {
-        border-radius: 4px;
+        margin-top: 20px;
+        border-radius: 10px;
     }
 
-    .bg-purple-dark {
-        background: #99a9bf;
-    }
-
-    .bg-purple {
-        background: #d3dce6;
-    }
-
-    .bg-purple-light {
-        background: #e5e9f2;
-    }
-
-    .grid-content {
-        border-radius: 4px;
+    /* 表格圆角 */
+    .grid-content, .el-carousel__container, .el-carousel__item, .el-table {
+        border-radius: 10px;
         min-height: 36px;
     }
 
-    .row-bg {
-        padding: 10px 0;
-        background-color: #f9fafc;
-    }
 </style>
--- src/view/main.vue
+++ src/view/main.vue
@@ -1,46 +1,62 @@
 <template>
     <div id="app">
         <el-container>
-            <el-header>
+            <!-- 头 -->
+            <el-header height="auto">
+                <!--
+                 mode：导航栏的样式，这里设置如果是移动端就显示折叠样式，pc端显示顶部导航样式
+                 default-active：当前选中
+                 background-color：背景色，绑定到embg
+                 active-text-color：选中时的颜色
+                 router：路由模式，这里如果开启就直接和vue的路由对接，index就是路由地址，否则每个菜单项都是a标签的形式
+                 @mouseenter.native：鼠标移入事件，给导航换了个颜色
+                 @mouseleave.native：移除也换个颜色
+                 -->
                 <el-menu
+                        :mode="cliWidth > 768 ? 'horizontal' : 'vertical'"
                         :default-active="activeIndex2"
                         class="el-menu-demo"
-                        mode="horizontal"
-                        @select="handleSelect"
-                        :background-color="embg"
+                        :background-color="embg ? 'black' : '#dabcbf'"
                         text-color="white"
                         active-text-color="#ffd04b"
-                        default-active="1"
-                        @mouseenter.native="embg='black'"
-                        @mouseleave.native="embg='transparent'"
                         router="true"
+                        @mouseenter.native="embg=!embg"
+                        @mouseleave.native="embg=!embg"
                 >
-                    <div style="float: left; width: 100px; color: red;">&nbsp;</div>
-                    <el-menu-item index="/">首页</el-menu-item>
-                    <el-submenu index="2">
+                    <!-- 当浏览器宽度大于768时显示pc端导航栏 -->
+                    <template v-if="cliWidth > 768">
+                        <el-menu-item index="/" v-navBarCss="el">首页</el-menu-item>
+                        <el-menu-item index="test" disabled>消息中心</el-menu-item>
+                        <el-menu-item index="test">test</el-menu-item>
+                        <li class="login">
+                            <el-button type="success">注册</el-button>
+                            <el-button type="primary">登录</el-button>
+                        </li>
+                    </template>
+
+                    <!-- 当浏览器宽度小于768时显示适配于移动端的导航栏 -->
+                    <el-submenu index="2" v-if="cliWidth < 768">
                         <template slot="title">我的工作台</template>
-                        <el-menu-item index="2-1">选项1</el-menu-item>
-                        <el-menu-item index="2-2">选项2</el-menu-item>
-                        <el-menu-item index="2-3">选项3</el-menu-item>
+                        <el-menu-item index="/">首页</el-menu-item>
+                        <el-menu-item index="test" disabled>消息中心</el-menu-item>
+                        <el-menu-item index="test">test</el-menu-item>
                     </el-submenu>
-                    <el-menu-item index="test" disabled>消息中心</el-menu-item>
-                    <el-menu-item index="test">test</el-menu-item>
-                    <div style="float: right; margin-right: 100px; color: red;">
-                        <h3>
-                            <a href="#">登录</a>
-                            &nbsp;|&nbsp;
-                            <a href="#">注册</a>
-                        </h3>
-                    </div>
+
                 </el-menu>
+
+
             </el-header>
 
+            <!-- 主体 -->
             <el-main>
-                <transition enter-active-class="animated zoomInDown" leave-active-class="animated slideOutDown" mode="out-in">
+                <!-- 动画包裹 -->
+                <transition enter-active-class="" leave-active-class="animated zoomOut" mode="out-in"
+                            :duration="{enter:730, leave:500}">
                     <router-view></router-view>
                 </transition>
             </el-main>
 
+            <!-- 尾部版权信息 -->
             <el-footer>Footer</el-footer>
         </el-container>
     </div>
@@ -49,30 +65,55 @@
     export default {
         created() {
         },
-        data() {
-            return {
-                height: '',
-                embg: 'transparent'
+        mounted() {
+            // 监听浏览器大小变化，将宽度实时赋值给vm的cliWidth
+            window.onresize = () => {
+                return (() => {
+                    this.cliWidth = document.body.clientWidth
+                })()
             }
         },
-        methods: {}
+        data() {
+            return {
+                // 导航栏的初始颜色
+                embg: false,
+                // 当前浏览器客户端的屏幕宽度
+                cliWidth: document.body.clientWidth
+            }
+        },
+        methods: {},
+        directives: {
+            // 自定义指令，当pc版导航栏被创建时给设置下导航栏第一个元素的左外边距
+            navBarCss: {
+                inserted: (el, xx, vnode) => {
+                    el.style.marginLeft = '100px'
+                }
+            }
+        }
     }
 </script>
 <style scoped>
-    .el-header, .el-footer {
-        text-align: center;
-        line-height: 60px;
-    }
-
-    .el-main {
-        color: #333;
+    .el-header, .el-footer, .el-main {
         text-align: center;
     }
 
+    /*背景图*/
     #app {
         background: url(../img/index_bg.png) no-repeat;
-        background-color: #354259;
+        background-color: #f8ffd2;
         height: 1202px;
     }
 
+    /*导航圆角*/
+    .el-menu,.el-submenu {
+        padding: 0 10px 0 10px;
+        border-radius: 10px;
+    }
+
+    .login {
+        margin-right: 100px;
+        text-align: center;
+        line-height: 60px;
+        float: right;
+    }
 </style>
